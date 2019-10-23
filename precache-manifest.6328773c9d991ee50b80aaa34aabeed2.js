55033397e4588681068033012ea3e44a5152506f
Aravindh-SNR <aravindhsnr@gmail.com>
2019-10-23 23:51:03 +0530

    Updates

---
+++ precache-manifest.6328773c9d991ee50b80aaa34aabeed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381aba3609d5cd964b5411e5cce75761",
+    "url": "/react-redux-todo-app/index.html"
+  },
+  {
+    "revision": "220b80b13bc3b93bb8b3",
+    "url": "/react-redux-todo-app/static/css/main.0d25266c.chunk.css"
+  },
+  {
+    "revision": "a2b96f1386981b409cad",
+    "url": "/react-redux-todo-app/static/js/2.fbfd142b.chunk.js"
+  },
+  {
+    "revision": "220b80b13bc3b93bb8b3",
+    "url": "/react-redux-todo-app/static/js/main.aa66635b.chunk.js"
+  },
+  {
+    "revision": "1d2237a3bbd12fc1a542",
+    "url": "/react-redux-todo-app/static/js/runtime-main.af77cfb1.js"
+  }
+]);
